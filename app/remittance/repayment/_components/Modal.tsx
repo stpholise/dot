0a055bc63a658f705fe17ed2c650001a62bec6db
stephen olise <stpholise@gmail.com>
2025-08-05 08:28:34 +0100

    add modal slide animation

--- app/remittance/repayment/_components/Modal.tsx
+++ app/remittance/repayment/_components/Modal.tsx
@@ -1,13 +1,25 @@
-import React from "react";
+'use client'
+import { useEffect, useState } from "react";
+import clsx from "clsx";
 
 interface ModalProp {
   setIsModalOpen: (state: boolean) => void;
 }
 
 const Modal = ({ setIsModalOpen }: ModalProp) => {
+  const [isVisible, setIsVisible] = useState<boolean>(false)
+  useEffect(() => {
+    setIsVisible(true) 
+     
+
+  },[])
+ 
   return (
     <>
-      <div className="w-full  lg:w-[600px] min-h-96 h-full z-80 fixed bottom-0 top-0 right-0 left-0 lg:left-auto bg-red overflow-y-scroll pb-6">
+      <div className={clsx("w-full bg-white lg:w-[600px] min-h-96 h-full z-80 fixed bottom-0 top-0 right-0 left-0 lg:left-auto bg-red overflow-y-scroll pb-6",{
+ ' transition transform translate-x-5 opacity-100 ease-in-out duration-500': isVisible,
+ ' transition transform -translate-x-100 opacity-0 ease-in-out duration-500': !isVisible
+      })}>
         Modal
       </div>
       <div
