770101f8ca94746d7c7f9e98cc5383011791decf
stephen olise <stpholise@gmail.com>
2025-08-08 21:51:58 +0100

    remittance details component

--- app/remittance/repayment/_components/Modal.tsx
+++ app/remittance/repayment/_components/Modal.tsx
@@ -86,13 +86,14 @@ const Modal = ({
     <>
       <div
         className={clsx(
-          "w-full bg-[#F9F9F9] lg:w-[600px]   z-80 fixed bottom-0 top-0 right-0 left-0 lg:left-auto bg-red overflow-y-scroll ",
+          "w-full bg-[#F9F9F9] lg:w-[600px] z-80 fixed bottom-0 top-0 right-0 left-0 lg:left-auto bg-red overflow-y-auto ",
           {
-            " transition transform translate-x-5 opacity-100 ease-in-out duration-500":
+            " transition-transform transform -translate-x-1 opacity-100 ease-in-out duration-500":
               isVisible,
-            " transition transform -translate-x-100 opacity-0 ease-in-out duration-500":
+            " transition transform translate-x-100 opacity-0 ease-in-out duration-500":
               !isVisible,
-            "h-fit ": selectedRowsItems?.length === 1,
+            "h-fit  top-1/2 right-1/2  transition-opacity translate-x-1/2 rounded-3xl  -translate-y-1/2 opacity-100 ease-in-out duration-500":
+              selectedRowsItems?.length === 1,
             "h-screen min-h-screen":
               selectedRowsItems?.length !== 1 || !selectedRowsItems,
           }
@@ -123,7 +124,12 @@ const Modal = ({
                 />
               </button>
             </div>
-            <div className="h-[calc(100vh-180px)] overflow-y-auto">
+            <div
+              className={clsx("h-[calc(100vh-180px)] overflow-y-auto", {
+                "h-fit ": selectedRowsItems?.length === 1,
+                "h-[calc(100vh-180px)] ": selectedRowsItems?.length !== 1,
+              })}
+            >
               <div className="xl:px-8 xl:py-6 md:px-6 pt-4 pb-12">
                 {selectedRowsItems && selectedRowsItems?.length !== 0 ? (
                   selectedRowsItems.map((item) => (
@@ -158,7 +164,11 @@ const Modal = ({
                       </div>
 
                       <div className=" flex justify-between gap-8 mt-4">
-                        <div className="border border-[#D2D5E1] w-full px-4 h-12 rounded-lg py-2 text-black flex items-center gap-1">
+                        <div
+                          className={
+                            "border border-[#D2D5E1] w-full px-4 h-12 rounded-lg py-2 text-black flex items-center gap-1"
+                          }
+                        >
                           ₦
                           <input
                             maxLength={7}
@@ -202,16 +212,31 @@ const Modal = ({
               </div>
             </div>
             <div className="sticky bottom-0 right-0 left-0 border border-[#EAEAEA] bg-white px-8 py-6 flex justify-between">
-              <div className="">
+              <div
+                className={clsx("", {
+                  "hidden ": selectedRowsItems?.length === 1,
+                  "block ": selectedRowsItems?.length !== 1,
+                })}
+              >
                 <h2 className="text-black">₦{totalRemittance}</h2>
                 <p className="text-xs font-medium text-[#667085]">
                   Total Remittance
                 </p>
               </div>
-              <div className=" flex gap-8 ">
+              <div
+                className={clsx("flex gap-8", {
+                  "gap-4  ": selectedRowsItems?.length === 1,
+                })}
+              >
                 <PrimaryButtons
                   text={`Cancel`}
-                  className="bg-white border border-[#D0D5DD] font-medium text-[#344054]  px-5 py-3 rounded-lg "
+                  className={clsx(
+                    "bg-white border border-[#D0D5DD] font-medium text-[#344054]  px-5 py-3 rounded-lg ",
+                    {
+                      "lg:w-50 flex item-center justify-center  ":
+                        selectedRowsItems?.length === 1,
+                    }
+                  )}
                   onClick={cancelRemittanceCreattion}
                 />
                 <PrimaryButtons
@@ -223,6 +248,10 @@ const Modal = ({
                   }
                   className={clsx(
                     "  px-5 py-3 rounded-lg text-white",
+                    {
+                      "lg:w-80 flex item-center justify-center  ":
+                        selectedRowsItems?.length === 1,
+                    },
                     buttonValidation ? "bg-black" : "bg-[#9A9A9A]"
                   )}
                   onClick={() => setCurrentModal(1)}
@@ -255,10 +284,117 @@ const Modal = ({
                 <Image
                   src={"/icons/close.svg"}
                   alt={"close modal"}
-                  width={14}
-                  height={14}
+                  width={24}
+                  height={24}
                 />
               </button>
+            </div>
+            <div className="h-[calc(100vh-180px)] ">
+              {
+                <div className="rounded-2xl border border-[#EAEAEA] ">
+                  <div className="  bg-[#F9F9F9] px-8 py-6 flex flex-col sm:flex-row  gap-8 justify-between rounded-t-3xl">
+                    <div className="text-black text-3xl font-semibold">
+                      <p className="text-sm text-#667085 font-medium">
+                        {" "}
+                        Total Customers
+                      </p>
+                      <div className="">
+                        {selectedRowsItems?.length} Customers
+                      </div>
+                    </div>
+                    <div className="">
+                      <p className="">Total Remittance Amount</p>
+                      <div className="">{totalRemittance}</div>
+                    </div>
+                  </div>
+                  <div className="">
+                    <h4 className="text-xs font-medium flex items-center">
+                      {" "}
+                      <Image
+                        src={"/icons/user.png"}
+                        alt={"customer"}
+                        width={8}
+                        height={10}
+                        className="inline mx-1"
+                      />
+                      CUSTOMER SUMMARY
+                    </h4>
+                    <div className="py-2">
+                      {selectedRowsItems &&
+                        selectedRowsItems.map((item) => (
+                          <div
+                            className="flex gap-4 border-b border-[#EAEAEA] py-4 justify-between items-center last:border-0"
+                            key={item.id}
+                          >
+                            <div className="">
+                              <h5 className=" text-black font-medium my-1">
+                                {item.customerName}
+                              </h5>
+                              <div className="flex gap-2 text-xs">
+                                <span className="">
+                                  <Image
+                                    src={"/icons/offer_hand.png"}
+                                    alt={"instalment"}
+                                    width={10}
+                                    height={10}
+                                    className="inline mx-1"
+                                  />
+                                  Instalment: {item.instalment}
+                                </span>
+                                <span>
+                                  <Image
+                                    src={"/icons/calender.png"}
+                                    alt={"instalment"}
+                                    width={10}
+                                    height={10}
+                                    className="inline mx-1"
+                                  />
+                                  Repayment: {item.repayment}
+                                </span>
+                              </div>
+                            </div>
+                            <p className="text-black text-2xl font-medium">
+                              ₦{rawValue[item.id]}
+                            </p>
+                          </div>
+                          // <div
+                          //   className="bg-white h-40  w-full lg:min-96 xl:px-8 px-4 sm:py-3 xl:py-6 mb-4 rounded-2xl"
+                          //   key={item.id}
+                          // >
+                          //   <p className="text-black text-2xl font-medium">
+                          //     {item.customerName}
+                          //   </p>
+                          //   <div className="flex gap-2 mt-1 text-xs text-[#667085]">
+                          //     <span className="">
+                          //       <Image
+                          //         src={"/icons/offer_hand.png"}
+                          //         alt={"instalment"}
+                          //         width={10}
+                          //         height={10}
+                          //         className="inline mx-1"
+                          //       />
+                          //       Instalment: {item.instalment}
+                          //     </span>
+                          //     <span>
+                          //       <Image
+                          //         src={"/icons/calender.png"}
+                          //         alt={"instalment"}
+                          //         width={10}
+                          //         height={10}
+                          //         className="inline mx-1"
+                          //       />
+                          //       Repayment: {item.repayment}
+                          //     </span>
+                          //   </div>
+                          //   <div className=" flex justify-between gap-8 mt-4">
+                          //     <div>₦{rawValue[item.id]}</div>
+                          //   </div>
+                          // </div>
+                        ))}
+                    </div>
+                  </div>
+                </div>
+              }
             </div>
             <div className="relative mt-auto bottom-0 right-0 left-0 border border-[#EAEAEA] bg-white px-8 py-6 flex justify-between lg:gap-8">
               <PrimaryButtons
